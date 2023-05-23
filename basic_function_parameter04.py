46cee29b2a8001832706ea2e3d28c4c1272cfcd9
Mardon1207 <mardonsultonov8@gmail.com>
2023-05-23 23:54:57 +0500

    4

--- basic_function_parameter04.py
+++ basic_function_parameter04.py
@@ -2,3 +2,11 @@
 # Inside the function, calculate the average of all the numbers in the given list.
 # Return the average.
 # Return the average.
+def calculate_average(suma):
+    i=0
+    s=0
+    while i<len(suma):
+        s+=suma[i]
+        i+=1
+    return s/len(suma)
+print(calculate_average(suma=[2,4,6,8]))
